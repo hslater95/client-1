51321caf6a819e72c1856d80919d193c9ea91d09
hslater95 <107321650+hslater95@users.noreply.github.com>
2022-09-14 14:03:04 -0700

    Update maincontent.jsx

--- components/maincontent.jsx
+++ components/maincontent.jsx
@@ -2,7 +2,7 @@ import styles from '../styles/Maincontent.module.css'
 
 function Maincontent() {
 return (<div className={styles.mcontent}>
-    <p className={styles.mcontent_text}>I am a full stack software developer located in Phoenix Arizona where I specalize in creating Restful API's, 
+    <p className={styles.mcontent_text}>I am a full stack software developer located in Phoenix Arizona where I specalize in creating Restful API&apos;s, 
     building event-driven architecture, and designing exceptional virtual experiences.</p>
 </div>)
 }
