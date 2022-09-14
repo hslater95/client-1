3d41239847b7eefd9b36af1b5caef506fa097ae8
hslater95 <107321650+hslater95@users.noreply.github.com>
2022-09-14 14:18:07 -0700

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Navbar.module.css'
 import Navlogo from '../components/navlogo'
-import Navtabs from '../components/navtabs';
+import Navtabs from '../components/NavTabs';
 import Emptyspace from '../components/emptyspace';
 
 function Navbar() {
